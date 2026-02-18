9f69b697042cc0fedf4a1aa8697c1a0e283ea89e
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-18 20:27:24 +0100

    problemi col responsive

--- assets/script.js
+++ assets/script.js
@@ -38,3 +38,50 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+//questo sara il nostro markup per le card 
+/*
+<div class="row">
+  <div class="col-4 card border-0 rounded-0">
+    <img src="./assets/img/male1.png" alt="">
+    <div class="card-body text-center">
+      <h2 class="card-title">MARCO BIANCHI</h2>
+      <div class="role fs-3">Designer</div>
+      <div class="email fs-3 text-primary">marcobianchi@team.com</div>
+    </div>
+  </div>
+</div>
+*/
+const rowEl=document.querySelector("section.team .col")
+//questo rendera il team in pagina 
+//creare la variabile d'appogio
+let card=""
+//ciclo for per far ciclare fino alla lunghezza dell'array per ogni elemento
+//tutto questo ci fa generare le card per ogni singolo elemento senza usare html
+for (let i = 0; i < teamMembers.length; i++) {
+  const singleMember = teamMembers[i];
+  const{name,role,email,img}=singleMember
+  console.log(singleMember);
+  const markup=`
+  <div class="row">
+  <div class="col-3 p-3">
+    <div class="card p-3">
+    <div class="card-body text-center">
+        <img class="card-img-top" src="./assets/${img}" alt="">
+        <h2 class="card-title">${name}</h2>
+        <div class="role fs-3">${role}</div>
+        <div class="email fs-3 text-primary">${email}</div>
+      </div>
+    </div>
+  </div>
+  </div>
+  `
+
+  console.log(markup);
+  
+  card += markup
+
+}
+
+console.log(card);
+rowEl.innerHTML = card
